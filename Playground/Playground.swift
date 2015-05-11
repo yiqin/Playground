71cbc6888ef9dc08e07a1664c59a72d3e2f04d7c
Yi Qin <yiqin.mems@gmail.com>
2015-05-10 19:09:09 -0500

    Add Playground

---
+++ Playground/Playground.swift
@@ -0,0 +1,13 @@
+//
+//  Playground.swift
+//  Playground
+//
+//  Created by Yi Qin on 5/10/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+import UIKit
+
+class Playground: NSParseObject {
+
+}
